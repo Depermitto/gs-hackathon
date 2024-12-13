7fd820f0838ba2ae144d8829595a25c7e454cd45
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2024-12-14 00:13:41 +0100

    some data retrieving, not checked

---
+++ frontend/src/components/BasicApiData.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const BasicApiData = () => {
+    const [data, setData] = React.useState<any>(null);
+
+    React.useEffect(() => {
+        fetch("http://localhost:8000")
+            .then((res) => res.json())
+            .then((data) => setData(data));
+    }, []);
+
+    return (
+        <div>
+            {data ? (
+                <div>
+                    <h1>{data.title}</h1>
+                    <p>{data.description}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default BasicApiData;
