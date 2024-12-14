542e960a678114418f129a39b7496607fc0fd393
Depermitto <penciller@disroot.org>
2024-12-14 05:29:58 +0100

    add api merger

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
 import injection
-import auth_check
+from . import auth_check
 
 
 app = FastAPI()
@@ -29,8 +28,15 @@ async def read_root() -> dict:
 async def scan() -> dict:
     yaml_path = "example.yaml"
     url, _ = injection.parse_endpoints.parse_yaml(yaml_path)
-    injection.pipeline(yaml_path, "injections.txt")
 
     raport = {}
+    raport["vulnerabilities"] = 0
+    raport["https"] = auth_check.check_https(url)
+    raport["cookie_secure"] = auth_check.check_cookie_secure(url)
+    raport["cookie_permanent"] = not auth_check.check_cookie_timeout(url)
+    raport["sql"] = injection.pipeline(yaml_path, "injections.txt")
+    for sql_raport in raport["sql"]:
+        raport["vulnerabilities"] += sql_raport["body_injection"]
+        raport["vulnerabilities"] += sql_raport["path_injection"]
 
     return raport
