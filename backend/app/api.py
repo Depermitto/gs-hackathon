e3d7aefef7c87417b6c8ce2a4b0f9f8285e24348
Depermitto <penciller@disroot.org>
2024-12-14 05:33:02 +0100

    properly count vulns

--- backend/app/api.py
+++ backend/app/api.py
@@ -31,9 +31,16 @@ async def scan() -> dict:
 
     raport = {}
     raport["vulnerabilities"] = 0
+
     raport["https"] = auth_check.check_https(url)
+    raport["vulnerabilities"] += not raport["https"]
+
     raport["cookie_secure"] = auth_check.check_cookie_secure(url)
+    raport["vulnerabilities"] += not raport["cookie_secure"]
+
     raport["cookie_permanent"] = not auth_check.check_cookie_timeout(url)
+    raport["vulnerabilities"] += raport["cookie_permanent"]
+
     raport["sql"] = injection.pipeline(yaml_path, "injections.txt")
     for sql_raport in raport["sql"]:
         raport["vulnerabilities"] += sql_raport["body_injection"]
