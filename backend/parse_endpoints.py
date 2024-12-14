90b0cf8f3ce649779e58386da93f26c6110f2183
Duszke333 <duszke333@gmail.com>
2024-12-14 02:00:24 +0100

    Now everything is in one structure

--- backend/parse_endpoints.py
+++ backend/parse_endpoints.py
@@ -2,33 +2,34 @@ import yaml
 
 
 def parse_yaml(path):
-    path_params = []
-    insecure = []
-    params = []
+    results = []
     with open("example.yaml", "r") as f:
         spec = yaml.safe_load(f)
     paths = spec.get("paths", {})
     for path, methods in paths.items():
         for method, details in methods.items():
-            if "{" in path:
-                path_params.append({"method": method, "path": path})
+            path_param = True if "{" in path else False
+            schema = None
             security = details.get("security", spec.get("security", []))
-            if not security:
-                insecure.append({"method": method, "path": path})
             request_body = details.get("requestBody")
             if request_body:
                 content = request_body.get("content", {})
                 for content_type in content.values():
                     schema = content_type.get("schema")
-                    if schema:
-                        params.append(
-                            {"method": method, "path": path, "schema": schema}
-                        )
-    return path_params, insecure, params
+            results.append(
+                {
+                    "method": method.upper(),
+                    "path": path,
+                    "secure": True if security else False,
+                    "path_param": path_param,
+                    "schema": schema,
+                }
+            )
+    return results
+
 
 if __name__ == "__main__":
-    path, ins, par = parse_yaml("example_yaml")
+    res = parse_yaml("example_yaml")
 
-    print(f"PATH:\n{path}\n")
-    print(f"INSECURE:\n{ins}\n")
-    print(f"PARAMS:\n{par}\n")
+    for r in res:
+        print(f"{r}\n")
