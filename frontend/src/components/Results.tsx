ff150e9fef4ea33daf56a219e8b385bd4adde739
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2024-12-14 09:52:39 +0100

    Formatted vulnerabilities

--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css'
 
-import { Title, Text, Stack, Group, Button } from '@mantine/core';
+import { Title, Text, Stack, Group, Button, Table } from '@mantine/core';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { IconCircleCheck, IconCircleX } from '@tabler/icons-react';
 
@@ -30,8 +30,55 @@ function Results() {
           <Text size="lg">
             {results.vulnerabilities
               ? `Found vulnerabilities: ${results.vulnerabilities}`
-              : 'No vulnerabilities found!'}
+              : 'No vulnerabilities found!'
+            }
           </Text>
+          {results.vulnerabilities && (
+              <Table>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>Type</Table.Th>
+                    <Table.Th>Description</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                  {results.https == false && (
+                    <Table.Tr>
+                      <Table.Td>HTTPS</Table.Td>
+                      <Table.Td>HTTPS protocol is not used</Table.Td>
+                    </Table.Tr>
+                  )}
+                  {results.cookie_permanent == true && (
+                    <Table.Tr>
+                      <Table.Td>Cookie</Table.Td>
+                      <Table.Td>Cookie does not have the Secure and HttpOnly flags set</Table.Td>
+                    </Table.Tr>
+                  )}
+                  {results.cookie_secure == false && (
+                    <Table.Tr>
+                      <Table.Td>Cookie</Table.Td>
+                      <Table.Td>Cookie does not have the Secure flag set</Table.Td>
+                    </Table.Tr>
+                  )}
+                  {results.sql && results.sql.map((item: any, index: any) => (
+                    <>
+                      {item.body_injection && (
+                        <Table.Tr key={`body-${index}`}>
+                          <Table.Td>{item.route}</Table.Td>
+                          <Table.Td>Body injection vulnerability detected</Table.Td>
+                        </Table.Tr>
+                      )}
+                      {item.path_injection && (
+                        <Table.Tr key={`path-${index}`}>
+                          <Table.Td>{item.route}</Table.Td>
+                          <Table.Td>Path injection vulnerability detected</Table.Td>
+                        </Table.Tr>
+                      )}
+                    </>
+                  ))}
+                </Table.Tbody>
+              </Table>
+          )}
         </Stack>
       ) : (
         <Text size="lg" c="red">
